0d017f84410e5e81345559287e9015cb9c6e6b73
saa22u842 <saa22u842@student.bmstu.ru>
2024-03-17 18:42:20 +0300

    hw-2

--- cmd/task_2/main.go
+++ cmd/task_2/main.go
@@ -1,16 +1,33 @@
 package main
 
+import (
+	"time"
+)
+
 // Здесь надо реализовать структуру User
 type User struct {
+	Name string
+	Login string
+	Password string
+	LastSeenAt time.Time
 }
 
 // Здесь надо реализовать структуру Admin, включающую в себя User
 type Admin struct {
+	User
+	Access uint8
 }
 
 // Здесь должна быть функция изменения имени пользователя
 func renameUser(user interface{}, newName string) {
-
+	switch v := user.(type){
+	case *User:
+		v.Name = newName
+	case *Admin:
+		if v.Access < 3{
+			v.Name = newName
+		}
+	}
 }
 
 func main() {
